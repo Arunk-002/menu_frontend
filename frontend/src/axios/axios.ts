e324f5f7588401e3d118eeacc83d80c6c950fa10
arun <arunkumarjack7892gmail.com>
2024-12-12 00:59:09 +0530

    frontend changed axios link changed

--- frontend/src/axios/axios.ts
+++ frontend/src/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://menu-fullstack.onrender.com", 
+  baseURL: "https://menu-fullstack.onrender.com/api", 
   headers: {
     "Content-Type": "application/json",
   },
